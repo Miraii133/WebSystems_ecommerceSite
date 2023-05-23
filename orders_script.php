f66990a6d517edb6c7cd6edccb813280ab0658b9
Miraii133 <jiyovalmoria@gmail.com>
2023-05-22 20:40:08 -0700

    add: new for loops to retrieve data.

--- orders_script.php
+++ orders_script.php
@@ -24,35 +24,54 @@ function connectToDB()
 function displayPendingOrders($dlink)
 {
 
-    $get_productDescription_sql = <<<SQL
-SELECT * FROM purchase WHERE status="pending";
-SQL;
 
-    $get_PendingOrders_sql = <<<SQL
+
+    $get_productDescription_sql = <<<SQL
     SELECT * FROM purchase WHERE status="pending";
     SQL;
+
+    $get_PendingOrders_sql = <<<SQL
+    SELECT userid, prodid FROM purchase WHERE status="pending";
+    SQL;
+
+    // retrieves all pending orders arrays and loops through 
+    // all to get Array objects. This in turn allows
+    // retrieval of all userid and prodid which
+    // has pending state
     $pendingOrders_result = mysqli_query($dlink, $get_PendingOrders_sql);
     foreach ($pendingOrders_result as $pendingOrders_rows) {
-        // if user viewing product.php is not logged in, remove add to cart button
-        $product_info_html = <<<HTML
+        $row_userid = $pendingOrders_rows['userid'];
+        $row_prodid = $pendingOrders_rows['prodid'];
+        // retrieves all the details with the specific row_userid
+        // and row_prodid
+        $get_allDataFrom_userIds_sql = <<<SQL
+        SELECT * FROM products WHERE prodid=$row_prodid;
+        SQL;
+        $specificOrders_details = mysqli_query($dlink, $get_allDataFrom_userIds_sql);
+        while ($rows = $specificOrders_details->fetch_assoc()) {
+            echo $rows['productname'];
+            echo $rows['ourprice'];
+            $product_info_html = <<<HTML
+          
 
- <tr> 
-        <td style="width: 0px; display:inline; margin-top:100px;">
-        <td style="width: 0px; display:inline; padding-left: 0px; padding-right: 0px; padding-bottom: 100px;"> 
-        <img src="img/product-1.png"> </td>
-        <td style="padding-left: 0px; padding-right: 0px; padding-bottom: 100px;"> 
-        <p> {$pendingOrders_rows['productname']} x{$pendingOrders_rows['productprice']}</p>
-    </td>
-        
-        <td style="padding-left: 100px; padding-right: 0px; padding-bottom: 100px;"> {$pendingOrders_rows['quantity']}</td>
-        <td style="padding-left: 100px; padding-right: 0px; padding-bottom: 100px;"> {$pendingOrders_rows['date']}</td>
-        <td style="padding-left: 100px; padding-right: 0px; padding-bottom: 100px;"> {$pendingOrders_rows['status']}</td>
+  <tr> 
+         <td style="width: 0px; display:inline; margin-top:100px;">
+         <td style="width: 0px; display:inline; padding-left: 0px; padding-right: 0px; padding-bottom: 100px;"> 
+         <img src="img/product-1.png"> </td>
+         <td style="padding-left: 0px; padding-right: 0px; padding-bottom: 100px;"> 
+         <p> {$rows['productname']} x{$rows['productprice']}</p>
+     </td>
+         
+         <td style="padding-left: 100px; padding-right: 0px; padding-bottom: 100px;"> {$pendingOrders_rows['quantity']}</td>
+         <td style="padding-left: 100px; padding-right: 0px; padding-bottom: 100px;"> {$pendingOrders_rows['date']}</td>
+         <td style="padding-left: 100px; padding-right: 0px; padding-bottom: 100px;"> {$pendingOrders_rows['status']}</td>
 
-   
-HTML;
-        echo $product_info_html;
-        // if user viewing product.php is logged in, add to cart button
+    
+ HTML;
+            echo $product_info_html;
+            // if user viewing product.php is logged in, add to cart button
 
+        }
     }
 }
 
@@ -67,7 +86,6 @@ function displayAcceptedOrders($dlink)
     SQL;
     $pendingOrders_result = mysqli_query($dlink, $get_PendingOrders_sql);
     foreach ($pendingOrders_result as $pendingOrders_rows) {
-        // if user viewing product.php is not logged in, remove add to cart button
         $product_info_html = <<<HTML
 
  <tr> 
@@ -167,7 +185,6 @@ if (isset($_REQUEST['status']) && $_REQUEST['status'] == 'pending') {
     displayPendingOrders($dlink);
 } else if (isset($_REQUEST['status']) && $_REQUEST['status'] == 'accepted') {
     displayAcceptedOrders($dlink);
-    echo "boo";
 }
 
 ?>
