d28039930262e1c7fc71d02977d0a8991bc37b2e
Miraii133 <jiyovalmoria@gmail.com>
2023-05-23 20:34:52 -0700

    add: implementation of product category filtering in product.php

--- product_display_script.php
+++ product_display_script.php
@@ -21,37 +21,25 @@ function connectToDB()
     return $dlink;
 }
 
-//function get
-
-function getAllAvailableProduct($dlink)
+function getDogFoodCategory($dlink, $prodcat)
 {
-    // retrieves DISTINCT prodcategories
-    // to avoid duplication of categories
-    $get_unique_category_query = "
-    SELECT DISTINCT prodcat 
-    FROM products";
 
-    $result = mysqli_query($dlink, $get_unique_category_query);
-    while ($row = mysqli_fetch_assoc($result)) {
-        $prod_cat_html = <<<HTML
-        <h1 class="text-center"> <a href="product.php?categorySelected={$row['prodcat']}"> {$row['prodcat']} </a></h1>;
-        HTML;
-        echo $prod_cat_html;
-        // gets all products that have the same category as the one being looped in $row
-        // example, if current category is dog_food, and the current product being looped
-        // has the category dog_food, then retrieve all the data of that row.
-        $product_info_query = "
-    SELECT prodid, productname, productdesc, productlink, 
-    productimage, quantity, lastprice, ourprice 
+    $prod_cat_html = <<<HTML
+            <h1 class="text-center"> $prodcat </a></h1>;
+            HTML;
+    echo $prod_cat_html;
+
+    $product_info_query = "
+    SELECT *
     FROM products 
-    WHERE prodcat='${row['prodcat']}'";
+    WHERE prodcat='${prodcat}'";
 
-        $product_info_result = mysqli_query($dlink, $product_info_query);
-        // loops through entire rows that matches the current category
-        foreach ($product_info_result as $product_row) {
-            // if user viewing product.php is not logged in, no add-to-cart button
-            if (!isset($_COOKIE['email'])) {
-                $product_info_html = <<<HTML
+    $product_info_result = mysqli_query($dlink, $product_info_query);
+    // loops through entire rows that matches the current category
+    foreach ($product_info_result as $product_row) {
+        // if user viewing product.php is not logged in, no add-to-cart button
+        if (!isset($_COOKIE['email'])) {
+            $product_info_html = <<<HTML
  <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
             <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
             <h6 class="text-uppercase">{$product_row['productname']}</h6>
@@ -62,10 +50,10 @@ function getAllAvailableProduct($dlink)
             </div>
         </div>
 HTML;
-                // if user viewing product.php is logged in, include add-to-cart button
-                // and if current product is not out of stock
-            } else if (isset($_COOKIE['email']) && $product_row['quantity'] > 0) {
-                $product_info_html = <<<HTML
+            // if user viewing product.php is logged in, include add-to-cart button
+            // and if current product is not out of stock
+        } else if (isset($_COOKIE['email']) && $product_row['quantity'] > 0) {
+            $product_info_html = <<<HTML
  <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
             <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
             <h6 class="text-uppercase">{$product_row['productname']}</h6>
@@ -91,9 +79,9 @@ HTML;
         </div>
 HTML;
 
-                // if current product to be displayed is empty
-            } else if (isset($_COOKIE['email']) && $product_row['quantity'] <= 0) { {
-                    $product_info_html = <<<HTML
+            // if current product to be displayed is empty
+        } else if (isset($_COOKIE['email']) && $product_row['quantity'] <= 0) { {
+                $product_info_html = <<<HTML
  <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
             <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
             <h6 class="text-uppercase">{$product_row['productname']}</h6>
@@ -106,24 +94,282 @@ HTML;
             </div>
         </div>
 HTML;
+            }
+        }
+        echo $product_info_html;
+    }
+}
+
+function getCatFoodCategory($dlink, $prodcat)
+{
+    $prod_cat_html = <<<HTML
+            <h1 class="text-center"> $prodcat </a></h1>;
+            HTML;
+    echo $prod_cat_html;
+
+    $product_info_query = "
+    SELECT *
+    FROM products 
+    WHERE prodcat='${prodcat}'";
+
+    $product_info_result = mysqli_query($dlink, $product_info_query);
+    // loops through entire rows that matches the current category
+    foreach ($product_info_result as $product_row) {
+        // if user viewing product.php is not logged in, no add-to-cart button
+        if (!isset($_COOKIE['email'])) {
+            $product_info_html = <<<HTML
+ <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
+            <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
+            <h6 class="text-uppercase">{$product_row['productname']}</h6>
+            <h5 class="text-primary mb-0">{$product_row['lastprice']}</h5>
+            <h6 class="text-primary mb-0">Quantity: {$product_row['quantity']}</h6>
+            <div class="btn-action d-flex justify-content-center">
+            <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-eye"></i></a>
+            </div>
+        </div>
+HTML;
+            // if user viewing product.php is logged in, include add-to-cart button
+            // and if current product is not out of stock
+        } else if (isset($_COOKIE['email']) && $product_row['quantity'] > 0) {
+            $product_info_html = <<<HTML
+ <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
+            <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
+            <h6 class="text-uppercase">{$product_row['productname']}</h6>
+            <h5 class="text-primary mb-0">{$product_row['lastprice']}</h5>
+            <h6 class="text-primary mb-0">Quantity: {$product_row['quantity']}</h6>
+            <div class="btn-action d-flex justify-content-center">
+
+            <!-- Creates HTML anchor that contains dynamic 
+                variable values from database and passes it to
+                cart_script.php -->
+                <a class="btn btn-primary py-2 px-3" href="cart_script.php?add_to_cart=true&
+                prodid={$product_row['prodid']}&
+                productdesc={$product_row['productdesc']}&
+                productname={$product_row['productname']}&
+                productimage={$product_row['productimage']}&
+                quantity={$product_row['quantity']}&
+                price={$product_row['lastprice']}
+                ">
+               
+                <i class="bi bi-cart"></i></a>
+                <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-eye"></i></a>
+            </div>
+        </div>
+HTML;
+
+            // if current product to be displayed is empty
+        } else if (isset($_COOKIE['email']) && $product_row['quantity'] <= 0) { {
+                $product_info_html = <<<HTML
+ <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
+            <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
+            <h6 class="text-uppercase">{$product_row['productname']}</h6>
+            <h5 class="text-primary mb-0">{$product_row['lastprice']}</h5>
+            <h6 class="text-primary mb-0">Quantity: OUT OF STOCK!</h6>
+            <div class="btn-action d-flex justify-content-center">
+
+               
+                <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-eye"></i></a>
+            </div>
+        </div>
+HTML;
+            }
+        }
+        echo $product_info_html;
+    }
+}
+
+function getCleaningEquipmentCategory($dlink, $prodcat)
+{
+
+    $prod_cat_html = <<<HTML
+            <h1 class="text-center"> $prodcat </a></h1>;
+            HTML;
+    echo $prod_cat_html;
+
+    $product_info_query = "
+    SELECT *
+    FROM products 
+    WHERE prodcat='${prodcat}'";
+
+    $product_info_result = mysqli_query($dlink, $product_info_query);
+    // loops through entire rows that matches the current category
+    foreach ($product_info_result as $product_row) {
+        // if user viewing product.php is not logged in, no add-to-cart button
+        if (!isset($_COOKIE['email'])) {
+            $product_info_html = <<<HTML
+ <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
+            <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
+            <h6 class="text-uppercase">{$product_row['productname']}</h6>
+            <h5 class="text-primary mb-0">{$product_row['lastprice']}</h5>
+            <h6 class="text-primary mb-0">Quantity: {$product_row['quantity']}</h6>
+            <div class="btn-action d-flex justify-content-center">
+            <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-eye"></i></a>
+            </div>
+        </div>
+HTML;
+            // if user viewing product.php is logged in, include add-to-cart button
+            // and if current product is not out of stock
+        } else if (isset($_COOKIE['email']) && $product_row['quantity'] > 0) {
+            $product_info_html = <<<HTML
+ <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
+            <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
+            <h6 class="text-uppercase">{$product_row['productname']}</h6>
+            <h5 class="text-primary mb-0">{$product_row['lastprice']}</h5>
+            <h6 class="text-primary mb-0">Quantity: {$product_row['quantity']}</h6>
+            <div class="btn-action d-flex justify-content-center">
+
+            <!-- Creates HTML anchor that contains dynamic 
+                variable values from database and passes it to
+                cart_script.php -->
+                <a class="btn btn-primary py-2 px-3" href="cart_script.php?add_to_cart=true&
+                prodid={$product_row['prodid']}&
+                productdesc={$product_row['productdesc']}&
+                productname={$product_row['productname']}&
+                productimage={$product_row['productimage']}&
+                quantity={$product_row['quantity']}&
+                price={$product_row['lastprice']}
+                ">
+               
+                <i class="bi bi-cart"></i></a>
+                <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-eye"></i></a>
+            </div>
+        </div>
+HTML;
+
+            // if current product to be displayed is empty
+        } else if (isset($_COOKIE['email']) && $product_row['quantity'] <= 0) { {
+                $product_info_html = <<<HTML
+ <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
+            <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
+            <h6 class="text-uppercase">{$product_row['productname']}</h6>
+            <h5 class="text-primary mb-0">{$product_row['lastprice']}</h5>
+            <h6 class="text-primary mb-0">Quantity: OUT OF STOCK!</h6>
+            <div class="btn-action d-flex justify-content-center">
+
+               
+                <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-eye"></i></a>
+            </div>
+        </div>
+HTML;
+            }
+        }
+        echo $product_info_html;
+    }
+}
+
+
+function getAllAvailableProduct($dlink)
+{
+    // retrieves DISTINCT prodcategories
+    // to avoid duplication of categories
+    $get_unique_category_query = "
+    SELECT DISTINCT prodcat 
+    FROM products";
+
+    $result = mysqli_query($dlink, $get_unique_category_query);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $prod_cat_html = <<<HTML
+            <h1 class="text-center"> <a href="product.php?categorySelected={$row['prodcat']}"> {$row['prodcat']} </a></h1>;
+            HTML;
+        echo $prod_cat_html;
+        // gets all products that have the same category as the one being looped in $row
+        // example, if current category is dog_food, and the current product being looped
+        // has the category dog_food, then retrieve all the data of that row.
+        $product_info_query = "
+            SELECT prodid, productname, productdesc, productlink, 
+            productimage, quantity, lastprice, ourprice 
+            FROM products 
+            WHERE prodcat='${row['prodcat']}'";
+
+        $product_info_result = mysqli_query($dlink, $product_info_query);
+        // loops through entire rows that matches the current category
+        foreach ($product_info_result as $product_row) {
+            // if user viewing product.php is not logged in, no add-to-cart button
+            if (!isset($_COOKIE['email'])) {
+                $product_info_html = <<<HTML
+     <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
+                <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
+                <h6 class="text-uppercase">{$product_row['productname']}</h6>
+                <h5 class="text-primary mb-0">{$product_row['lastprice']}</h5>
+                <h6 class="text-primary mb-0">Quantity: {$product_row['quantity']}</h6>
+                <div class="btn-action d-flex justify-content-center">
+                <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-eye"></i></a>
+                </div>
+            </div>
+    HTML;
+                // if user viewing product.php is logged in, include add-to-cart button
+                // and if current product is not out of stock
+            } else if (isset($_COOKIE['email']) && $product_row['quantity'] > 0) {
+                $product_info_html = <<<HTML
+     <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
+                <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
+                <h6 class="text-uppercase">{$product_row['productname']}</h6>
+                <h5 class="text-primary mb-0">{$product_row['lastprice']}</h5>
+                <h6 class="text-primary mb-0">Quantity: {$product_row['quantity']}</h6>
+                <div class="btn-action d-flex justify-content-center">
+
+                <!-- Creates HTML anchor that contains dynamic 
+                    variable values from database and passes it to
+                    cart_script.php -->
+                    <a class="btn btn-primary py-2 px-3" href="cart_script.php?add_to_cart=true&
+                    prodid={$product_row['prodid']}&
+                    productdesc={$product_row['productdesc']}&
+                    productname={$product_row['productname']}&
+                    productimage={$product_row['productimage']}&
+                    quantity={$product_row['quantity']}&
+                    price={$product_row['lastprice']}
+                    ">
+                   
+                    <i class="bi bi-cart"></i></a>
+                    <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-eye"></i></a>
+                </div>
+            </div>
+    HTML;
+
+                // if current product to be displayed is empty
+            } else if (isset($_COOKIE['email']) && $product_row['quantity'] <= 0) { {
+                    $product_info_html = <<<HTML
+     <div class="product-item position-relative  bg-light d-inline-flex flex-column text-center">
+                <img class="rounded mx-auto d-block" src="{$product_row['productimage']}" alt="">
+                <h6 class="text-uppercase">{$product_row['productname']}</h6>
+                <h5 class="text-primary mb-0">{$product_row['lastprice']}</h5>
+                <h6 class="text-primary mb-0">Quantity: OUT OF STOCK!</h6>
+                <div class="btn-action d-flex justify-content-center">
+
+                   
+                    <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-eye"></i></a>
+                </div>
+            </div>
+    HTML;
                 }
             }
             echo $product_info_html;
 
         }
-
-
-
     }
-    if ($_GET['categorySelected'] == 'Food') {
-        print_r("food");
-    } else if ($_GET['categorySelected'] == 'Cleaning Equipment') {
-        print_r("cleaning");
-    } else if ($_GET['categorySelected'] == 'Bird food') {
-        print_r("bird_food");
-    }
+
+
+
+
 }
 
 $dlink = connectToDB();
-echo getAllAvailableProduct($dlink);
+if (
+    isset($_GET['categorySelected']) &&
+    $_GET['categorySelected'] == 'Dog_food'
+) {
+    getDogFoodCategory($dlink, $_GET['categorySelected']);
+} else if (
+    isset($_GET['categorySelected'])
+    && $_GET['categorySelected'] == 'Cleaning_Equipment'
+) {
+    getCleaningEquipmentCategory($dlink, $_GET['categorySelected']);
+} else if (
+    isset($_GET['categorySelected'])
+    && $_GET['categorySelected'] == 'Cat_food'
+) {
+    getCatFoodCategory($dlink, $_GET['categorySelected']);
+} else {
+    echo getAllAvailableProduct($dlink);
+}
 ?>
